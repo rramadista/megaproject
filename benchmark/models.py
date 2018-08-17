d51facaf3883a0575fbe7c009aab625a22ccb34a
Raden Ramadista <raden.ramadista@gmail.com>
2018-08-17 15:50:41 +0700

    Added upload image method.

--- benchmark/models.py
+++ benchmark/models.py
@@ -4,14 +4,31 @@ from djmoney.models.fields import MoneyField
 
 # Create your models here.
 class Bank(models.Model):
+    OWNERSHIP = (
+        ('1', 'Stated Owned Bank'),
+        ('2', 'Regional Bank'),
+        ('3', 'Private Forex Bank'),
+        ('4', 'Private Non-Forex Bank'),
+        ('5', 'Joint Venture Bank'),
+        ('6', 'Foreign Bank'),
+    )
+    BUKU = (
+        ('1', '1'),
+        ('2', '2'),
+        ('3', '3'),
+        ('4', '4'),
+    )
     institution_name = models.CharField(max_length=100)
     bank_name = models.CharField(max_length=50)
+    logo = models.ImageField(upload_to='logo', blank=True, null=True)
     tin = models.CharField(max_length=50, blank=True, null=True)
     swift_code = models.CharField(max_length=50, blank=True, null=True)
     est_date = models.DateField(blank=True, null=True)
     forex_date = models.DateField(blank=True, null=True)
     listing_date = models.DateField(blank=True, null=True)
-    website = models.CharField(max_length=100, blank=True, null=True)
+    website = models.URLField(max_length=100, blank=True, null=True)
+    category = models.CharField(max_length=1, choices=OWNERSHIP, blank=True, null=True)
+    group = models.CharField(max_length=1, choices=BUKU, blank=True, null=True)
     
     def __str__(self):
         return self.bank_name
@@ -23,10 +40,10 @@ class Indicator(models.Model):
     funding = MoneyField(max_digits=19, decimal_places=4, default_currency='IDR')
     lending = MoneyField(max_digits=19, decimal_places=4, default_currency='IDR')
     asset = MoneyField(max_digits=19, decimal_places=4, default_currency='IDR')
-    headcount = models.IntegerField()
+    headcount = models.PositiveIntegerField()
 
     def __str__(self):
-        return '%s %d' % (self.period, self.bank)
+        return '%s %s' % (self.period, self.bank)
 
 class DimDate(models.Model):
     full_date = models.DateField(default=timezone.now, null=True)
--- benchmark/views.py
+++ benchmark/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from .models import Bank
+from .models import Bank, Indicator
 
 # Create your views here.
 def index(request):
